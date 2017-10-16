df7632dbd90b01d262c0a4202ca36f900338b7ff
André Monteiro <andremonteiro-@hotmail.com>
2017-10-16 19:53:28 +0100

    Query Methods

--- TBD/QueryMethods.cs
+++ TBD/QueryMethods.cs
@@ -2,24 +2,50 @@
 {
     public class QueryMethods
     {
+        private const string COLUMN_NOME = "Nome";
+        private const string COLUMN_FATURA = "FacturaID";
+        private const string COLUMN_CLIENT = "ClientID";
+        private const string COLUMN_MORADA = "Morada";
+
         public static string CreateRandomTransaction(string isolationLevel)
         {
             string tran;
             tran = "SET TRANSACTION ISOLATION LEVEL " + isolationLevel + " ";
-            tran += ""; 
+            tran += "";
             // TODO
 
             return tran;
         }
 
-        public static string CreateTransaction(string isolationLevel, string query)
+        public static string GenerateUpdateTransaction(string isolationLevel, string fatura_id, string cliente_id, string nome, string morada)
         {
             string tran;
-            tran = "SET TRANSACTION ISOLATION LEVEL " + isolationLevel + " ";
-            tran += "BEGIN TRAN ";
-            tran += query + " ";
-            tran += "COMMIT;";
-            // TODO : ROLLBACK?
+            tran = "UPDATE " + Config.DEFAULT_TABLENAME + " ";
+            tran += "SET " + COLUMN_CLIENT + " = " + cliente_id + ", ";
+            tran += "" + COLUMN_NOME + " = " + nome + ", ";
+            tran += "" + COLUMN_MORADA + " = " + morada + " ";
+            tran += "WHERE " + COLUMN_FATURA + " = " + fatura_id;
+
+            return tran;
+        }
+
+        public static string GenerateDeleteTransaction(string isolationLevel, string fatura_id)
+        {
+            string tran;
+            tran = "DELETE FROM " + Config.DEFAULT_TABLENAME + " ";
+            tran += "WHERE " + COLUMN_FATURA + " = " + fatura_id;
+
+            return tran;
+        }
+
+        public static string GenerateInsertTransaction(string isolationLevel, string fatura_id, string cliente_id, string nome, string morada)
+        {
+            string tran;
+            tran = "INSERT INTO " + Config.DEFAULT_TABLENAME + "( " + COLUMN_FATURA + ", " + COLUMN_CLIENT + ", " + COLUMN_NOME + ", " + COLUMN_MORADA + ") ";
+            tran += "VALUES (" + fatura_id + ", ";
+            tran += "" + cliente_id + ", ";
+            tran += "" + nome + ", ";
+            tran += "" + morada + ")";
 
             return tran;
         }
